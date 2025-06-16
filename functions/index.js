497a582c3254916f67a47c266c05fd8e8aa718fe
yatte shop <yatteshop@gmail.com>
2025-06-16 16:04:48 +0000

    vike 2

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,18 @@ const { renderPage } = require("vike");
 
 exports.handler = async (event, context) => {
   const pageContext = await renderPage({ urlOriginal: event.rawUrl });
-  if (!pageContext.httpResponse) return { statusCode: 200 };
+
+  if (!pageContext.httpResponse) {
+    return { statusCode: 200, body: "" };
+  }
+
   console.log(pageContext.httpResponse.statusCode, event.rawUrl);
 
   return {
     statusCode: pageContext.httpResponse.statusCode,
-    headers: { "Content-Type": pageContext.httpResponse.contentType },
+    headers: {
+      "Content-Type": pageContext.httpResponse.contentType,
+    },
     body: pageContext.httpResponse.body,
   };
 };
