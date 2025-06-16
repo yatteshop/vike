c4a42b22d732619b98ffcaba6e15c0f447e24b6f
yatte shop <yatteshop@gmail.com>
2025-06-16 16:32:37 +0000

    vike 5

--- netlify/functions/ssr.js
+++ netlify/functions/ssr.js
@@ -1,13 +1,27 @@
-const { renderPage } = require("vike");
+const { renderPage } = require("vike/server");
 
-exports.handler = async (event, context) => {
-  const pageContext = await renderPage({ urlOriginal: event.rawUrl });
-  if (!pageContext.httpResponse) return { statusCode: 200 };
-  console.log(pageContext.httpResponse.statusCode, event.rawUrl);
+exports.handler = async (event) => {
+  try {
+    const pageContext = await renderPage({ urlOriginal: event.rawUrl });
 
-  return {
-    statusCode: pageContext.httpResponse.statusCode,
-    headers: { "Content-Type": pageContext.httpResponse.contentType },
-    body: pageContext.httpResponse.body,
-  };
+    if (!pageContext.httpResponse) {
+      return {
+        statusCode: 200,
+        body: '',
+      };
+    }
+
+    const { statusCode, contentType, body } = pageContext.httpResponse;
+
+    return {
+      statusCode,
+      headers: { 'Content-Type': contentType },
+      body,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: 'Erreur serveur : ' + err.message + '\n\n' + err.stack,
+    };
+  }
 };
